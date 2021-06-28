a89d475037eeb90b8d02696697fea83e601f1371
Jakeedot <86273620+Jakeedot@users.noreply.github.com>
2021-06-28 16:17:48 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -19,7 +19,7 @@ How to use me? 👉 /help
    Video  - Give File as video without Screenshots
    DFile  - Give File without Screenshots
 
-Join: @DotBots
+**Join: @DotBots**
 """
 
     FORMAT_SELECTION = """Select the desired format: <a href='{}'>file size might be approximate</a>
@@ -37,7 +37,7 @@ URL | newfilename | username | password"""
     
     UPLOAD_START = "Uploading now.."
     
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \n\nUploaded in {} seconds.        Join **@DotBots**"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \n\nUploaded in {} seconds.                  Join **@DotBots**"
 
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.95GB due to Telegram API limitations."
 
