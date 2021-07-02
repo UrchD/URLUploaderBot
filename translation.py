0623a99cde83562967479860fa7b5aa6965f211d
Jakeedot <86273620+Jakeedot@users.noreply.github.com>
2021-07-02 12:45:10 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,6 +1,6 @@
 class Translation(object):
 
-    START_TEXT = """Hello <username>,
+    START_TEXT = """Hello,
 
 <b>Please send me any Direct download URL link with extention, I can upload to telegram as File/Video</b>
 
@@ -19,7 +19,7 @@ How to use me? 👉 /help
    Video  - Give File as video without Screenshots
    DFile  - Give File without Screenshots
 
-**Join: @DotBots**
+**Join:👉 @DotBots**
 """
 
     FORMAT_SELECTION = """Select the desired format: <a href='{}'>file size might be approximate</a>
@@ -31,13 +31,13 @@ You can use /deletethumbnail to delete the auto-generated thumbnail."""
 URL | newfilename | username | password"""
 
 
-    UPGRADE_TEXT = "<b>👉 Create own Clone Bot.. </b>  \n\n<a href='https://github.com/prgofficial/URLuploader-With-Hotstar'>Click here, Fork and deploy!!</a>"
+    UPGRADE_TEXT = "<b>👉 Create own Clone Bot.. </b>  \n\n<a href='https://github.com/JakeeDot/URLUploaderBot'>Click here, Fork and deploy!!</a>"
     
     DOWNLOAD_START = "Trying to download your file..."
     
     UPLOAD_START = "Uploading now.."
     
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \n\nUploaded in {} seconds.                  Join **@DotBots**"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \n\nUploaded in {} seconds. \n\n Join: 👉**@DotBots**"
 
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.95GB due to Telegram API limitations."
 
@@ -49,7 +49,7 @@ URL | newfilename | username | password"""
 
     SLOW_URL_DECED = "Gosh that seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file. Meanwhile, why don't you try this:==> https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
 
-    NO_VOID_FORMAT_FOUND = "ERROR...\n<b>YouTubeDL</b> said: {}"
+    NO_VOID_FORMAT_FOUND = "ERROR...\n<b>Not Supported</b>: {}"
     
     SHOW_THUMB = "Use /deletethumbnail to clear this thumbnail."
     
